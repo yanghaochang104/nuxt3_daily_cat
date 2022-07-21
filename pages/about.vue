d1d858442a73598015792fe5b912762097a561ae
ray.chang <yanghaochang1004@gmail.com>
2022-07-21 14:02:35 +0800

    feat(cats): add cats category

--- pages/about.vue
+++ pages/about.vue
@@ -4,19 +4,15 @@
     <p>興趣使然的愛貓工程師</p>
   </div>
 </template>
-<script>
-export default {
-  head() {
-    return {
-      title: "About the App",
-      meta: [
-        {
-          hid: "description",
-          name: "description",
-          content: "stories about web engineer and cats",
-        },
-      ],
-    };
-  },
-};
+<script setup>
+useHead({
+  title: "About the App",
+  meta: [
+    {
+      hid: "description",
+      name: "description",
+      content: "stories about web engineer and cats",
+    },
+  ],
+});
 </script>
---
+++ pages/cats/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Cat-binet</h1>
+    <div v-for="(cat, index) in cats" :key="cat.id">
+      <nuxt-link :to="'cats/' + cat.id">
+        <img
+          :src="cat.url"
+          :width="400"
+          :height="(cat.height * 400) / cat.width"
+        />
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+<script setup>
+useHead({
+  title: "Cat-binet",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  meta: [
+    {
+      hid: "description",
+      name: "description",
+      content: "cat, cat and ... more cats！",
+    },
+  ],
+});
+const { data: cats } = await useFetch(
+  "https://api.thecatapi.com/v1/images/search?limit=5&page=10&order=desc"
+);
+</script>
